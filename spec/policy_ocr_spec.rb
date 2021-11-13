97467ecb2e44e44c9366e99ef736722f7c0ce4a1
MandyWade0312 <amanda.wade@kin.com>
2021-11-13 13:19:46 -0500

    rspec test written

--- spec/policy_ocr_spec.rb
+++ spec/policy_ocr_spec.rb
@@ -8,4 +8,19 @@ describe PolicyOcr do
   it 'loads the sample.txt' do
     expect(fixture('sample').lines.count).to eq(44)
   end
+
+  describe "make_a_policy_number" do 
+    context "given 'sample'" do 
+      it "makes a policy number from sample" do
+        array = 
+        [
+          000000000, 111111111, 222222222, 333333333, 
+          444444444, 555555555, 666666666, 777777777, 
+          888888888, 999999999, 123456789      
+        ]
+        expect(PolicyOcr.make_a_policy_number('spec/fixtures/sample.txt')).to eq(array)
+      end
+    end   
+  end
+
 end
